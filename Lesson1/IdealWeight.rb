0b01f33553b59e2da4ab5807cf02bb301b032a74
Syanom <yandupliy@gmail.com>
2021-09-15 21:12:53 +0300

    resolve ideal weight part

---
+++ Lesson1/IdealWeight.rb
@@ -0,0 +1,12 @@
+puts "What is your name?"
+name = gets.chomp
+puts "What is your height?"
+height = gets.chomp.to_i
+
+ideal_weight = (height - 110) * 1.15
+
+if ideal_weight < 0 
+	puts "Your weight is optimal already"
+else 
+	puts "#{name}, your ideal weight is #{ideal_weight}"
+end
