133eb49af20867bd1ccf5bf82810a70315f0b93e
Syanom <yandupliy@gmail.com>
2021-09-15 22:06:43 +0300

    make right triangle

---
+++ Lesson1/RightTriangle.rb
@@ -0,0 +1,27 @@
+puts "Enter first side of triangle, please"
+a = gets.chomp.to_i
+puts "Enter second side of triangle, please"
+b = gets.chomp.to_i
+puts "Enter third side of triangle, please"
+c = gets.chomp.to_i
+
+# Make c the biggest side, like in trigonometry
+if a > b && a > c
+	tmp = a 
+	a = c
+	c = tmp
+elsif b > a && b > c
+	tmp = b
+	b = c
+	c = tmp
+end
+
+if a == b && a == c
+	puts "Triangle is equilateral"
+elsif a == b 
+	puts "Triangle is isosceles"
+elsif c ** 2 == a ** 2 + b ** 2
+	puts "Triangle is right-angled"
+else
+	puts "Triangle is not right-angled"
+end
