bc59e0499b28d7b6318e543fd2b219b0c2a1c2f6
Syanom <yandupliy@gmail.com>
2021-09-15 21:23:16 +0300

    Add square of triangle

---
+++ Lesson1/TriangleS.rb
@@ -0,0 +1,7 @@
+puts "Enter triangle's base, please"
+base = gets.chomp.to_i
+puts "Enter triangle's height, please"
+height = gets.chomp.to_i
+
+Square = 0.5 * base * height
+puts "Square = #{Square}"
