8ad183fdfc7cbd60bc832888d197580c0c71522a
Syanom <yandupliy@gmail.com>
2021-09-19 16:38:24 +0300

    make wagon child classes

---
+++ Lesson4/cargo_wagon.rb
@@ -0,0 +1,7 @@
+require_relative 'wagon.rb'
+
+class CargoWagon
+  def initialize
+    super(:cargo)
+  end
+end
---
+++ Lesson4/passenger_wagon.rb
@@ -0,0 +1,7 @@
+require_relative 'wagon.rb'
+
+class PassengerWagon
+  def initialize
+    super(:passenger)
+  end
+end
