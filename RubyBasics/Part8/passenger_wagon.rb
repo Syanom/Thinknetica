9b21e561d0168b48fbbe016f39e52db8f5d14552
Syanom <yandupliy@gmail.com>
2021-09-30 15:36:05 +0300

    make seats methods

--- RubyBasics/Part8/passenger_wagon.rb
+++ RubyBasics/Part8/passenger_wagon.rb
@@ -1,7 +1,20 @@
 require_relative 'wagon.rb'
 
 class PassengerWagon < Wagon
-  def initialize
+  attr_reader :seats
+  attr_reader :taken_seats
+
+  def initialize(seats)
+    @seats = seats
+    @taken_seats = 0
     super("passenger")
+  end
+
+  def take_seat
+    @taken_seats += 1
+  end
+
+  def seats_left?
+    @seats - @taken_seats
   end
 end
