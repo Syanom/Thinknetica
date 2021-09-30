327888032769194e8a16ee502aee33b7ac92b93c
Syanom <yandupliy@gmail.com>
2021-09-30 15:40:42 +0300

    make volume methods

--- RubyBasics/Part8/cargo_wagon.rb
+++ RubyBasics/Part8/cargo_wagon.rb
@@ -1,7 +1,20 @@
 require_relative 'wagon.rb'
 
 class CargoWagon < Wagon
-  def initialize
+  attr_reader :volume
+  attr_reader :taken_volume
+
+  def initialize(volume)
+    @volume = volume
+    @taken_volume = 0
     super("cargo")
+  end
+
+  def take_volume(volume)
+    @taken_volume += volume
+  end
+
+  def volume_left?
+    @volume - @taken_volume
   end
 end
